b90ca313af731c9000a6de8ddf6efdb923cc18d2
Leonardo Rocha <=>
2023-03-24 16:25:44 -0300

    cors policy applied

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express, {Express, Request, Response} from "express";
 import * as dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors({origin: '*'}));
 app.use("/user", require("./routes/UserRoute"));
-app.use("/menuitem", require("./routes/MenuItemRoute"));
-app.use("/restaurant", require("./routes/RestaurantRoute"));
+//app.use("/menuitem", require("./routes/MenuItemRoute"));
+//app.use("/restaurant", require("./routes/RestaurantRoute"));
 app.use("/login", require("./routes/LoginRoute"));
 
-app.listen(port, () => console.log(`listing server on ${port}!`));
+app.listen(port, () => console.log(`listening server on ${port}!`));
